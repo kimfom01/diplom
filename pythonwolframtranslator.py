d7195726a8a46b7de3042ba05a93e93cb1f6606d
Kim Fom <kimfom01@gmail.com>
2022-10-18 14:52:57 +0300

    add members of class and test output

--- pythonwolframtranslator.py
+++ pythonwolframtranslator.py
@@ -1,4 +1,36 @@
+from wolframclient.evaluation import WolframLanguageSession
+session = WolframLanguageSession()
+
 class PyWolfTranslator:
-    def __init__(self, equation):
-        self.equation = equation
+    def __init__(self, equation, symbol):
+        self._equation = equation
+        self._symbol = symbol
     
+    @property
+    def equation(self):
+        return self._equation
+    
+    @property
+    def symbol(self):
+        return self._symbol
+    
+    def evaluate_equation(self):
+        eq_form = f"{self._equation}"
+        sym_form = self._symbol
+        expr = "With[{eq = " + f"{eq_form}" + "}, " + f"{sym_form}]"
+        result_temp = session.evaluate_wrap(expr)
+        tmp = session.evaluate_wrap("ToString[TeXForm[{}]]".format(result_temp.result[0][0][1]))
+        tmp_result = str(tmp)
+        ind = tmp_result.index('=') + 1
+        ind_end = tmp_result.index('>')
+        result_to_latex = tmp_result[ind:ind_end]
+        return result_to_latex
+
+
+
+eq = 'x^2 + a x + 1 == 0'
+symbol = 'Solve[eq, x]'
+mytest = PyWolfTranslator(eq, symbol)
+print(mytest.equation)
+result = mytest.evaluate_equation()
+print(result)
