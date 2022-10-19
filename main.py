1ce45c9591facfb28d24c0a0b9395cbc8c7947ae
Kim Fom <kimfom01@gmail.com>
2022-10-19 16:05:05 +0300

    more stuff

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from pythonwolframtranslator import PyWolfTranslator
 
-eq = 'Derivative[2][x][t] - (3 t) + Derivative[1][x][t] == 0'
-symbol = 'DSolve[eq, x[t], t]'
+# eq = 'Derivative[2][x][t] - (3 t) + Derivative[1][x][t] == 0'
+# symbol = 'DSolve[eq, x[t], t]'
+
+eq = 'x^2 + 2 x - 15 = 0'
+symbol = 'Solve[eq, x]'
+
 mytest = PyWolfTranslator(eq, symbol)
-# print(mytest.equation)
 result = mytest.evaluate_equation()
+
 print(result)
--- pythonwolframtranslator.py
+++ pythonwolframtranslator.py
@@ -19,8 +19,11 @@ class PyWolfTranslator:
         sym_form = self._symbol
         expression = "With[{eq = " + f"{eq_form}" + "}, " + f"{sym_form}]"
         result_temp = session.evaluate_wrap(expression)
-        print(result_temp.result[0][0][1]) ## don't forget to delete
-        tmp = session.evaluate_wrap("ToString[TeXForm[{}]]".format(result_temp.result[0][0][1]))
+        # print(result_temp) ## don't forget to delete
+        # self.print_res((result_temp.result[0])) ## don't forget to delete
+        val = result_temp.result
+        tmp = session.evaluate_wrap("ToString[TeXForm[{}]]".format(val))
+        # tmp = session.evaluate_wrap("ToString[TeXForm[{}]]".format(self.__flatten_to_list(result_temp.result[0]))) 
         print(tmp) ## don't forget to delete
         tmp_result = str(tmp)
         ind = tmp_result.index('=') + 1
@@ -29,5 +32,21 @@ class PyWolfTranslator:
         self.__close_session()
         return result_to_latex
     
+    def print_res(self, res):
+        print(self.__flatten_to_list(res))
+    
+    def __flatten_to_list(self, tuple_value):
+        li = []
+        for test in tuple_value:
+            if type(test) is tuple:
+                for item in test:
+                    if type(item) is tuple:
+                        li.extend(item)
+                    else:
+                        li.append(item)
+            else:
+                li.append(test)
+        return li
+
     def __close_session(self):
         session.stop()
