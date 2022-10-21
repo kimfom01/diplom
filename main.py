ec5cee00b0c6215cde6dab3c59ae2249dab6c6f1
Kim Fom <kimfom01@gmail.com>
2022-10-21 10:42:01 +0300

    finally made it to run

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 from pythonwolframtranslator import PyWolfTranslator
 
 # eq = 'Derivative[2][x][t] - (3 t) + Derivative[1][x][t] == 0'
-# symbol = 'DSolve[eq, x[t], t]'
+# symbol = 'DSolveValue'
+# params = 'x[t], t'
 
-eq = 'x^2 + 2 x - 15 = 0'
-symbol = 'Solve[eq, x]'
+# eq = 'x^2 + 2 x - 15 == 0'
+# symbol = 'Roots'
+# params = 'x'
 
-mytest = PyWolfTranslator(eq, symbol)
+# eq = 'x^2 + 2 x + 1'
+# symbol = f'Factor[{eq}]'
+
+# eq = 'x^2 + 3 x - 4 == 0'
+# symbol = 'Roots'
+# params = 'x'
+
+# eq = '(x^3 - 1)/(x - 1)'
+# symbol = 'Limit'
+# params = 'x -> 1'
+
+# eq = 'x^6'
+# symbol = f'D[{eq}, x]'
+# params = 'x'
+
+# eq = '8 x^4'
+# symbol = f'Integrate[{eq}, x]'
+# params = 'x'
+
+eq = 'y\'[x] + y[x] == x'
+symbol = f'DSolveValue[{eq}, y[x], x]'
+params = 'x'
+
+# eq = 'Sin[x + I y]'
+# symbol = f'ComplexExpand[{eq}]'
+
+# eq = 'E^(I x)'
+# symbol = f'ExpToTrig[{eq}]'
+
+mytest = PyWolfTranslator(eq, symbol, params)
 result = mytest.evaluate_equation()
 
 print(result)
--- pythonwolframtranslator.py
+++ pythonwolframtranslator.py
@@ -1,10 +1,12 @@
 from wolframclient.evaluation import WolframLanguageSession
+from sympy.parsing.mathematica import parse_mathematica
 session = WolframLanguageSession()
 
 class PyWolfTranslator:
-    def __init__(self, equation, symbol):
+    def __init__(self, equation, symbol, parameters=''):
         self._equation = equation
         self._symbol = symbol
+        self._parameters = parameters
     
     @property
     def equation(self):
@@ -13,40 +15,21 @@ class PyWolfTranslator:
     @property
     def symbol(self):
         return self._symbol
+
+    @property
+    def parmeters(self):
+        return self._parameters
     
     def evaluate_equation(self):
-        eq_form = f"{self._equation}"
-        sym_form = self._symbol
-        expression = "With[{eq = " + f"{eq_form}" + "}, " + f"{sym_form}]"
-        result_temp = session.evaluate_wrap(expression)
-        # print(result_temp) ## don't forget to delete
-        # self.print_res((result_temp.result[0])) ## don't forget to delete
-        val = result_temp.result
-        tmp = session.evaluate_wrap("ToString[TeXForm[{}]]".format(val))
-        # tmp = session.evaluate_wrap("ToString[TeXForm[{}]]".format(self.__flatten_to_list(result_temp.result[0]))) 
-        print(tmp) ## don't forget to delete
-        tmp_result = str(tmp)
-        ind = tmp_result.index('=') + 1
-        ind_end = tmp_result.index('>')
-        result_to_latex = tmp_result[ind:ind_end]
+        eq = f"{self._equation}"
+        if self.parmeters == '':
+            sym_form = f'{self._symbol}[{eq}]'
+        else:
+            sym_form = f'{self._symbol}[{eq}, {self._parameters}]'
+        result = session.evaluate(sym_form)
+        result_to_latex = session.evaluate(f'ToString[TeXForm[{result}]]')
         self.__close_session()
         return result_to_latex
-    
-    def print_res(self, res):
-        print(self.__flatten_to_list(res))
-    
-    def __flatten_to_list(self, tuple_value):
-        li = []
-        for test in tuple_value:
-            if type(test) is tuple:
-                for item in test:
-                    if type(item) is tuple:
-                        li.extend(item)
-                    else:
-                        li.append(item)
-            else:
-                li.append(test)
-        return li
 
     def __close_session(self):
         session.stop()
