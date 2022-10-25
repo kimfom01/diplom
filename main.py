a2aeff3e1c822913ade292c57ccb342bbcf2b9b4
Kim Fom <kimfom01@gmail.com>
2022-10-25 11:40:55 +0300

    remove some equations

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from pythonwolframtranslator import PyWolfTranslator
 from auth import consumer_key, consumer_secret
 
-# eq = 'Derivative[2][x][t] - (3 t) + Derivative[1][x][t] == 0'
-# symbol = 'DSolveValue'
-# var = 'x[t], t'
-
-# eq = 'x^2 + 2 x - 15 == 0'
-# symbol = 'Roots'
-# var = 'x'
-
 # eq = 'x^2 + 2 x + 1'
 # symbol = 'Factor'
+
+# eq = 'x^2 + 3 x - 4 == 0'
+# symbol = 'Roots'
+# var = 'x'
 
 # eq = '(x^3 - 1)/(x - 1)'
 # symbol = 'Limit'
@@ -24,19 +20,12 @@ from auth import consumer_key, consumer_secret
 # symbol = 'Integrate'
 # var = 'x'
 
-# eq = 'y\'[x] + y[x] == x'
-# symbol = 'DSolveValue'
-# var = 'x'
+eq = 'y\'[x] + y[x] == x'
+symbol = 'DSolveValue'
+var = 'y[x], x'
 
 # eq = 'Sin[x + I y]'
 # symbol = 'ComplexExpand'
-
-# eq = 'E^(I x)'
-# symbol = 'ExpToTrig'
-
-eq = 'x^2 + 3 x - 4 == 0'
-symbol = 'Roots'
-var = 'x'
 
 mytest = PyWolfTranslator(consumer_key, consumer_secret)
 result = mytest.evaluate_equation(eq, symbol, var)
