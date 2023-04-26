6d445bb7b93fe1839cea29e78a31465f82f4e190
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-04-26 17:47:13 +0300

    Add generator files

---
+++ chaotic_generator.py
@@ -0,0 +1,41 @@
+from generator import generate_data
+import numpy as np
+
+
+def calculate_eigen_values(lines):
+    eigen_values = []
+
+    with open("data.csv") as file:
+        for _ in range(lines):
+            line = file.readline()
+            line_array = line.split(",")
+            array = np.array(
+                [[int(line_array[0]), int(line_array[1])],
+                 [int(line_array[2]),
+                  int(line_array[3].replace("\n", ""))]])
+            value, _ = np.linalg.eig(array)
+            eigen_values.append(value)
+    return eigen_values
+
+
+def write_output(eigen_values):
+    with open("output.txt", "w") as output:
+        for value in eigen_values:
+            found = False
+            for num in value:
+                if num >= 0:
+                    found = True
+                    break
+            if found:
+                output.write("1\n")
+            else:
+                output.write("0\n")
+
+
+lines = 1000
+
+generate_data(lines)
+
+eigen_values = calculate_eigen_values(lines)
+
+write_output(eigen_values)
---
+++ generator.py
@@ -0,0 +1,23 @@
+import csv
+import random
+
+
+def __get_random(lower, upper):
+    return [
+        random.randint(lower, upper),
+        random.randint(lower, upper),
+        random.randint(lower, upper),
+        random.randint(lower, upper)
+    ]
+
+
+def generate_data(lines):
+    with open("data.csv", "w") as output_file:
+        writer = csv.writer(output_file)
+        for i in range(lines):
+            row = __get_random(-9, 9)
+            writer.writerow(row)
+
+
+if __name__ == "__main__":
+    generate_data()
