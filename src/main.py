419897a1726230f7838b4945ee0991772a494069
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-04-28 09:58:36 +0300

    Build equation and calculate

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from core.pythonwolframtranslator import PyWolfTranslator
 from auth import consumer_key, consumer_secret
 
+csv_path = "./out/data.csv"
+
 
 def test_runner(key, secret, equation, symbol, variable=None):
     mytest = PyWolfTranslator(key, secret)
@@ -10,12 +12,35 @@ def test_runner(key, secret, equation, symbol, variable=None):
     return mytest.evaluate_equation(equation, symbol, variable)
 
 
-eq = '{x\'[t] == -y[t] - x[t]^2, y\'[t] == 2 x[t] - y[t]^3, x[0] == y[0] == 1}'
-sym = 'NDSolveValue'
-var = '{x, y}, {t, 0, 20}'
+def calculate_eigen_values(lines):
+    result = []
+    with open(csv_path) as file:
+        for _ in range(lines):
+            line = file.readline()
+            line = line[:-1]
+            matrix = line.split(",")
+            eq = f'x\'[t] == -{matrix[0]} y[t] - {matrix[1]} x[t]^2, y\'[t] == {matrix[2]} x[t] - {matrix[3]} y[t]^3, x[0] == y[0] == 1'
+            eq = '{' + eq + '}'
+            sym = 'NDSolveValue'
+            var = '{x, y}, {t, 0, 20}'
+            result.append(test_runner(
+                consumer_key, consumer_secret, eq, sym, var))
+    return result
 
-result = test_runner(consumer_key, consumer_secret, eq, sym, var)
-print(result)
+
+lines = 1000
+
+values = calculate_eigen_values(lines)
+
+print(values)
+
+
+# eq = '{x\'[t] == -y[t] - x[t]^2, y\'[t] == 2 x[t] - y[t]^3, x[0] == y[0] == 1}'
+# sym = 'NDSolveValue'
+# var = '{x, y}, {t, 0, 20}'
+
+# result = test_runner(consumer_key, consumer_secret, eq, sym, var)
+# print(result)
 
 # eq = 'x^2 + 2 x + 1'
 # sym = 'Factor'
