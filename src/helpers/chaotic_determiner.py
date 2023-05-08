bdd4ef2a6dba1244e9289a787b17c93957855a80
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-05-09 02:16:56 +0300

    Rename and refactor helper files
    Generate matrix and save with heading
    Modify chaotic determiner to skip heading when reading matrix file
    Add header and eigen values to results file

--- src/helpers/chaotic_determiner.py
+++ src/helpers/chaotic_determiner.py
@@ -1,4 +1,5 @@
-from generator import generate_data, csv_path
+from matrix_generator import generate_matrices, csv_path
+import csv
 import numpy as np
 
 
@@ -6,6 +7,8 @@ def calculate_eigen_values(lines):
     eigen_values = []
 
     with open(csv_path) as file:
+        _ = file.readline()
+
         for _ in range(lines):
             line = file.readline()
             line_array = line.split(",")
@@ -19,8 +22,15 @@ def calculate_eigen_values(lines):
 
 
 def write_output(eigen_values):
-    with open("../out/output.txt", "w") as output:
+    headers = ["eigen_values", "results"]
+
+    with open("../out/results.txt", "w") as output:
+        head = csv.DictWriter(output, delimiter=";", fieldnames=headers)
+        head.writeheader()
+
         for value in eigen_values:
+            output.write(str(list(map(str, value))))  # probably remove
+            output.write(";")
             found = False
             for num in value:
                 if num >= 0:
@@ -32,9 +42,9 @@ def write_output(eigen_values):
                 output.write("0\n")
 
 
-lines = 1000
+lines = 1001
 
-generate_data(lines)
+generate_matrices(lines)
 
 eigen_values = calculate_eigen_values(lines)
 
--- src/helpers/matrix_generator.py
+++ src/helpers/matrix_generator.py
@@ -1,7 +1,7 @@
 import csv
 import random
 
-csv_path = "../out/data.csv"
+csv_path = "../out/matrices.csv"
 
 
 def __get_random(lower, upper):
@@ -13,13 +13,18 @@ def __get_random(lower, upper):
     ]
 
 
-def generate_data(lines):
+def generate_matrices(lines):
+    headers = ["a[0]", "a[1]", "a[2]", "a[3]"]
+
     with open(csv_path, "w") as output_file:
-        writer = csv.writer(output_file)
-        for i in range(lines):
+        head = csv.DictWriter(output_file, delimiter=",", fieldnames=headers)
+        head.writeheader()
+
+        body = csv.writer(output_file)
+        for _ in range(lines):
             row = __get_random(-9, 9)
-            writer.writerow(row)
+            body.writerow(row)
 
 
 if __name__ == "__main__":
-    generate_data()
+    generate_matrices()
