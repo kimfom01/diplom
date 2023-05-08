fcfc0cc841ebdb9330c189f50060b411d33a26f7
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-05-09 02:23:06 +0300

    Refactor main to read from new matrices csv file and write to output.txt

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from core.pythonwolframtranslator import PyWolfTranslator
 from auth import consumer_key, consumer_secret
 
-csv_path = "./out/data.csv"
+csv_path = "./out/matrices.csv"
 
 
-def test_runner(key, secret, equation, symbol, variable=None):
+def solve_equation(key, secret, equation, symbol, variable=None):
     mytest = PyWolfTranslator(key, secret)
 
     if variable is None:
@@ -14,16 +14,19 @@ def test_runner(key, secret, equation, symbol, variable=None):
 
 def calculate_eigen_values(lines):
     result = []
+
     with open(csv_path) as file:
+        _ = file.readline()
+
         for _ in range(lines):
             line = file.readline()
-            line = line[:-1]
+            line = line.replace("\n", "")
             matrix = line.split(",")
             eq = f'x\'[t] == -{matrix[0]} y[t] - {matrix[1]} x[t]^2, y\'[t] == {matrix[2]} x[t] - {matrix[3]} y[t]^3, x[0] == y[0] == 1'
             eq = '{' + eq + '}'
             sym = 'NDSolveValue'
             var = '{x, y}, {t, 0, 20}'
-            result.append(test_runner(
+            result.append(solve_equation(
                 consumer_key, consumer_secret, eq, sym, var))
     return result
 
@@ -32,7 +35,10 @@ lines = 1000
 
 values = calculate_eigen_values(lines)
 
-print(values)
+values = str(list(map(str, values)))
+
+with open("output.txt", "w") as final:
+    final.writelines(values)
 
 
 # eq = '{x\'[t] == -y[t] - x[t]^2, y\'[t] == 2 x[t] - y[t]^3, x[0] == y[0] == 1}'
